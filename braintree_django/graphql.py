3735d78bc8d3326dc831c0c4840956e41fef2150
Lloyd <lloyd@capson.ca>
2022-07-25 17:18:50 -0600

    code clean

--- braintree_django/graphql.py
+++ braintree_django/graphql.py
@@ -9,18 +9,6 @@ from gql.transport.aiohttp import AIOHTTPTransport
 from .settings import braintree_settings
 
 
-class BraintreePaymentMethod:
-    payment_method: Dict
-    verification: Dict
-
-    def __init__(self, result: Dict):
-        self.payment_method = data.vaultPaymentMethod.payment_method
-        self.verification = data.vaultPaymentMethod.verification
-
-    def id(self) -> str:
-        return self.payment_method.id
-
-
 class BraintreeGraphQL:
     def __init__(self):
         base64_token = b64encode('{}:{}'.format(
@@ -50,70 +38,6 @@ class BraintreeGraphQL:
             transport=transport,
             fetch_schema_from_transport=False
         )
-
-    def client_token(self, customer_id: str):
-        query = gql(
-            """
-            mutation ClientToken($merchant: CreateClientTokenInput) {
-                createClientToken(input: $merchant) {
-                    clientToken
-                }
-            }
-            """
-        )
-        params = {
-            "merchant": {
-                "clientToken": {
-                    "merchantAccountId": braintree_settings.BRAINTREE_MERCHANT_ID,
-                    "customerId": customer_id
-                }
-            }
-        }
-
-        result = self._client.execute(query, variable_values=params)
-
-        return result
-
-    def save_payment_method(self, customer_id: str, nonce: str = None) -> BraintreePaymentMethod:
-        """
-        Use nonce to save payment method to vault.
-
-        Returns a BraintreePaymentMethod data object.
-        """
-        query = gql(
-            """
-            mutation VaultPaymentMethod($input: VaultPaymentMethodInput!) {
-                vaultPaymentMethod(input: $input) {
-                    paymentMethod {
-                    id
-                    usage
-                    details {
-                        __typename
-                        ... on CreditCardDetails {
-                        cardholderName
-                        last4      
-                        expirationMonth
-                        expirationYear 
-                        }
-                        
-                    }
-                    }
-                    verification {
-                    status
-                    }
-                }
-            }
-            """
-        )
-        params = {
-            "input": {
-                "paymentMethodId": nonce,
-                "customerId": customer_id
-            }
-        }
-        result = self._client.execute(query, variable_values=params)
-
-        return BraintreePaymentMethod(result)
 
 
 class BraintreeCustomer:
@@ -177,5 +101,113 @@ class BraintreeCustomer:
         }
 
         result = self.gql._client.execute(query, variable_values=params)
-        self.id = result["createCustomer"]["customer"]["id"]
+        self.id = result['createCustomer']['customer']['id']
         return self.id
+
+
+class BraintreePaymentMethod:
+    # payment_method: Dict
+    # verification: Dict
+
+    def __init__(
+        self,
+        customer_id: str,
+        method_id: str = None,
+        cardholder_name: str = None,
+        last_four: str = None,
+        exp_month: str = None,
+        exp_year: str = None,
+        gql: BraintreeGraphQL = None
+    ):
+        # self.payment_method = data.vaultPaymentMethod.payment_method
+        # self.verification = data.vaultPaymentMethod.verification
+        self.gql = gql
+        self.customer_id = customer_id
+        self.method_id = method_id
+        self.cardholder_name = cardholder_name
+        self.last_four = last_four
+        self.exp_month = exp_month
+        self.exp_year = exp_year
+
+        self.connect()
+
+    def connect(self):
+        if self.gql == None:
+            self.gql = BraintreeGraphQL()
+
+    def vault_payment_method(self, nonce: str = None):
+        """
+        Use nonce to save payment method to vault.
+
+        Returns a BraintreePaymentMethod data object.
+        """
+        query = gql(
+            """
+            mutation VaultPaymentMethod($input: VaultPaymentMethodInput!) {
+                vaultPaymentMethod(input: $input) {
+                    paymentMethod {
+                    id
+                    usage
+                    details {
+                        __typename
+                        ... on CreditCardDetails {
+                        cardholderName
+                        last4      
+                        expirationMonth
+                        expirationYear 
+                        }
+                        
+                    }
+                    }
+                    verification {
+                    status
+                    }
+                }
+            }
+            """
+        )
+        params = {
+            "input": {
+                "paymentMethodId": nonce,
+                "customerId": self.customer_id
+            }
+        }
+        result = self.gql._client.execute(query, variable_values=params)
+
+        payment_method = result['vaultPaymentMethod']['paymentMethod']
+
+        self.method_id = payment_method['id']
+        self.cardholder_name = payment_method['details']['cardholderName']
+        self.last_four = payment_method['details']['last4']
+        self.exp_month = payment_method['details']['expirationMonth']
+        self.exp_year = payment_method['details']['expirationYear']
+
+        return result
+
+    def get_client_token(self):
+        query = gql(
+            """
+            mutation ClientToken($merchant: CreateClientTokenInput) {
+                createClientToken(input: $merchant) {
+                    clientToken
+                }
+            }
+            """
+        )
+        params = {
+            "merchant": {
+                "clientToken": {
+                    "merchantAccountId": braintree_settings.BRAINTREE_MERCHANT_ID,
+                    "customerId": self.customer_id
+                }
+            }
+        }
+
+        result = self.gql._client.execute(query, variable_values=params)
+
+        self.client_token = result['createClientToken']['clientToken']
+
+        return self.client_token
+
+    def id(self) -> str:
+        return self.payment_method.id
