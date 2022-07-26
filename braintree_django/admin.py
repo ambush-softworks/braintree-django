92fa96263f2ae16ba171dfa45b8c46f27aad321f
Lloyd <lloyd@capson.ca>
2022-07-26 12:19:29 -0600

    make payment methods read_only

--- braintree_django/admin.py
+++ braintree_django/admin.py
@@ -6,6 +6,9 @@ from .models import PaymentMethod, CustomerVault
 class PaymentMethodInline(admin.StackedInline):
     model = PaymentMethod
 
+    def has_change_permission(self, request, obj=None):
+        return False
+
 
 class CustomerVaultAdmin(admin.ModelAdmin):
     list_display = ('customer_id', 'owner')
